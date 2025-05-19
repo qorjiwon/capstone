acfd0fee3004d007b45afd3fdf5f075e050c1b3d
qorjiwon <wldnjsl2001@khu.ac.kr>
2025-05-19 21:02:32 +0900

    change a tag to Link

--- app/pages/NotFound.tsx
+++ app/pages/NotFound.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react';
 import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 const NotFound = () => {
   const pathname = usePathname();
@@ -15,9 +16,9 @@ const NotFound = () => {
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">404</h1>
         <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
-        <a href="/" className="text-blue-500 hover:text-blue-700 underline">
+        <Link href="/" className="text-blue-500 hover:text-blue-700 underline">
           Return to Home
-        </a>
+        </Link>
       </div>
     </div>
   );
